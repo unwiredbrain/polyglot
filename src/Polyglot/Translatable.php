61bccdafdba546452d7e306774e5d4d4b0249773
unwiredbrain <unwiredbrain@gmail.com>
2013-05-09 18:17:49 +0100

    Rationalized the role of the shared interface.
    Now both Polyglot and the Context instances share a Translatable interface.

--- src/Polyglot/Translatable.php
+++ src/Polyglot/Translatable.php
@@ -14,15 +14,24 @@ namespace Polyglot;
 /**
  * @author Massimo Lombardo <unwiredbrain@gmail.com>
  */
-interface PolyglotInterface
+interface Translatable
 {
+
+    /**
+     * Interpolates a given sentence using the specified parameters.
+     *
+     * @param string $sentence A sentence to translate.
+     * @param array $params A key/value list of parameters to interpolate.
+     * @return string The interpolated sentence.
+     */
+    public function interpolate($sentence, array $params);
 
     /**
      * Translates a given sentence.
      *
      * @param string $sentence A sentence to translate.
      * @param array $params A key/value list of parameters to interpolate.
-     * @return string A ResultInterface result object.
+     * @return string The translated sentence.
      */
     public function translate($sentence, array $params = array());
 
